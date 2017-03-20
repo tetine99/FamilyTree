935670da6e0e66f6c4bb03e5a373e890fae4b078
DESKTOP-RS4DVHC\PC-Portable <raphael.gallardo@laposte.net>
2017-03-20 15:50:54 +0100

    TreeController et view associée terminée

---
+++ src/DL/FamilytreeBundle/Controller/TreeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DL\FamilytreeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class TreeController extends Controller
+{
+
+    /**
+     * @Route("/tree/view")
+     */
+    public function defaultViewAction()
+    {
+        return $this->render('DLFamilytreeBundle:Default:defaultTree.html.twig');
+    }
+
+}
