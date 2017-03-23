ca7ce035ebcd494ed5f35eaa87eb464de8ca6394
bunnarong <bunnarong.eap@gmail.com>
2017-03-23 14:26:02 +0100

    image

--- src/DL/FamilytreeBundle/Controller/PeopleController.php
+++ src/DL/FamilytreeBundle/Controller/PeopleController.php
@@ -9,112 +9,109 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\File\File;
 use DL\FamilytreeBundle\Form\Type\PeopleFormType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use DL\FamilytreeBundle\Entity\People;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
 
-class PeopleController extends Controller {
+class PeopleController extends Controller{
 
-    /**
-     * @Route("/people", name="people")
-     */
-    public function indexAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $peoples = $em->getRepository('DLFamilytreeBundle:People')
-                ->findAll();
+	/**
+	*@Route("/people", name="people")
+	*/
+	public function indexAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+		$peoples = $em->getRepository('DLFamilytreeBundle:People')
+			->findAll();
 
-        return $this->render('DLFamilytreeBundle:People:index.html.twig', [
-                    'peoples' => $peoples
-        ]);
-    }
-
-    /**
-     * @Route("/people/add", name="people_add")
-     * @Method({"GET", "POST"})
-     */
-    public function addAction(Request $request)
-    {
-        $people = new People();
-
-        $form = $this->createForm(PeopleFormType::class, $people);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid())
-        {
-
-            $file = $people->getImage();
-
-            if (!is_Null($file))
-            {
-                // Generate a unique name for the file before saving it
-                $fileName = md5(uniqid()) . '.' . $file->guessExtension();
-                // Move the file to the directory where brochures are stored
-                $file->move(
-                        $this->getParameter('img_directory'), $fileName
-                );
-                // Update the 'brochure' property to store the PDF file name
-                // instead of its contents
-                $people->setImage($fileName);
-            }
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($people);  //constitue l'objet
-            $em->flush();         //enregistre en bdd
-            // $pm = $this->get('Magasin.ProduitManager');
-            // $pm->saveProduct($produit);
-
-            return $this->redirectToRoute('people');
-        }
-        return $this->render('DLFamilytreeBundle:People:add.html.twig', [
-                    'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/people/update/{id}", name="people_update", defaults={"id"=null})
-     * @Method({"GET", "POST"})
-     */
-    public function updateAction(Request $request)
-    {
-        $id = $request->get('id');
-
-        $em = $this->getDoctrine()->getManager();
-        $people = $em->getRepository('DLFamilytreeBundle:People')->find($id);
-
-        //  $picture->setImage(
-        //    new File($this->getParameter('img_directory').'/'.$picture->getImage())
-        //  );
-        //  $pm = $this->get('Magasin.ProduitManager');
-        //  $produit = $pm->_productById($id);
-
-        $form = $this->createForm(PeopleFormType::class, $people);
+		return $this->render('DLFamilytreeBundle:People:index.html.twig', [
+			'peoples'=>$peoples
+		]);
+	}
 
 
-        $form->handleRequest($request);
-        if ($form->isValid())
-        {
-            $data = $form->getData();
-            //  $pm->saveProduct($produit);
-            $em->flush();
-            return $this->redirectToRoute('people');
-        }
-        return $this->render('DLFamilytreeBundle:people:update.html.twig', [
-                    'form' => $form->createView()
-        ]);
-    }
+	/**
+	*@Route("/people/add", name="people_add")
+	*@Method({"GET", "POST"})
+	*/
+	public function addAction(Request $request)
+	{
+		$people = new People();
 
-    /**
-     * @Route("/people/delete/{id}", name="people_delete", defaults={"id"=null})
-     */
-    public function deleteAction(Request $request)
-    {
-        $id = $request->get('id');
-        $em = $this->getDoctrine()->getManager();
-        $people = $em->getRepository('DLFamilytreeBundle:People')->find($id);
+		$form = $this->createForm(PeopleFormType::class, $people);
 
-        $em->remove($people);
-        $em->flush();
-        return $this->redirectToRoute('people');
-    }
+		$form->handleRequest($request);
+			if($form->isSubmitted() && $form->isValid()){
+
+				// $file = $people->getImage();
+
+				// if (!is_Null($file)){
+				//   // Generate a unique name for the file before saving it
+				//   $fileName = md5(uniqid()).'.'.$file->guessExtension();
+				//   // Move the file to the directory where brochures are stored
+				//   $file->move(
+				//      $this->getParameter('img_directory'),
+				//      $fileName
+				//   );
+				//   // Update the 'brochure' property to store the PDF file name
+				//   // instead of its contents
+				//   $people->setImage($fileName);
+				//  }
+
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($people);  //constitue l'objet
+				$em->flush();         //enregistre en bdd
+
+				return $this->redirectToRoute('people');
+			}
+		return $this->render('DLFamilytreeBundle:People:add.html.twig',[
+			'form'=>$form->createView()
+		]);
+	}
+
+
+	/**
+	 * @Route("/people/update/{id}", name="people_update", defaults={"id"=null})
+	 * @Method({"GET", "POST"})
+	 */
+	 public function updateAction(Request $request)
+	 {
+		 $id = $request->get('id');
+
+		 $em = $this->getDoctrine()->getManager();
+		 $people = $em->getRepository('DLFamilytreeBundle:People')->find($id);
+
+		 $form = $this->createForm(PeopleFormType::class, $people);
+
+		 $form->handleRequest($request);
+			 if($form->isValid()) {
+			 $data = $form->getData();
+
+			 $file = $people->getImageFile();
+			 $em->flush();
+
+			return $this->redirectToRoute('people');
+
+		 }
+		 return $this->render('DLFamilytreeBundle:people:update.html.twig', [
+				 'form' => $form->createView()
+		 ]);
+	 }
+
+	 /**
+		* @Route("/people/delete/{id}", name="people_delete", defaults={"id"=null})
+		*/
+	 public function deleteAction(Request $request)
+	 {
+		 $id = $request->get('id');
+		 $em = $this->getDoctrine()->getManager();
+		 $people = $em->getRepository('DLFamilytreeBundle:People')->find($id);
+
+		 $em->remove($people);
+		 $em->flush();
+		 return $this->redirectToRoute('people');
+	 }
+
+
 
 }
--- src/DL/FamilytreeBundle/Entity/People.php
+++ src/DL/FamilytreeBundle/Entity/People.php
@@ -3,12 +3,17 @@
 namespace DL\FamilytreeBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * People
  *
  * @ORM\Table(name="people")
  * @ORM\Entity(repositoryClass="DL\FamilytreeBundle\Repository\PeopleRepository")
+ * @Vich\Uploadable
+ * @ORM\HasLifecycleCallbacks
  */
 class People
 {
@@ -38,9 +43,32 @@ class People
     /**
      * @var string
      *
-     * @ORM\Column(name="image", type="string", nullable=true)
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
      */
     private $image;
+
+    /**
+    * NOTE: This is not a mapped field of entity metadata, just a simple property.
+    *
+    *@Assert\File(
+    *   maxSize="2M",
+    *   mimeTypes={"image/jpeg", "image/jpg", "image/png"},
+    *   mimeTypesMessage = "Please upload a valid picture"
+    *)
+    * @Vich\UploadableField(mapping="product_image", fileNameProperty="image")
+    *
+    * @var File $image
+    */
+   private $imageFile;
+
+
+
+   /**
+    * @var \DateTime
+    *
+    * @ORM\Column(name="updateat", type="datetime", nullable=true)
+    */
+   private $updatedAt;
 
 
     /**
@@ -112,6 +140,10 @@ class People
     {
         $this->image = $image;
 
+        if ($image instanceof UploadedFile) {
+            $this->setUpdatedAt(new \DateTime());
+        }
+
         return $this;
     }
 
@@ -125,8 +157,39 @@ class People
         return $this->image;
     }
 
-    public function getLabel()
-    {
-        return $this->firstname . ' ' . $this->lastname;
-    }
+
+    /**
+    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+    * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+    * must be able to accept an instance of 'File' as the bundle will inject one here
+    * during Doctrine hydration.
+    *
+    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+    *
+    * @return People
+    */
+   public function setImageFile(File $image = null)
+   {
+       $this->imageFile = $image;
+
+       if ($image) {
+           // It is required that at least one field changes if you are using doctrine
+           // otherwise the event listeners won't be called and the file is lost
+           $this->updatedAt = new \DateTimeImmutable();
+       }
+
+       return $this;
+   }
+
+   /**
+    * @return File|null
+    */
+   public function getImageFile()
+   {
+       return $this->imageFile;
+   }
+
+
+
 }
