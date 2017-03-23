3e42bdabd9cc69e43ccff1766566bdd6716807b6
tetine99 <tetine99@hotmail.Fr>
2017-03-23 11:09:51 +0100

    envoie e-mail et modif page login

--- src/DL/UserBundle/Controller/UserController.php
+++ src/DL/UserBundle/Controller/UserController.php
@@ -70,7 +70,7 @@ class UserController extends Controller {
 
         return $this->render('DLUserBundle:User:del.html.twig', [
                     'user' => $user,
-                    'form' => $form2->createView(),
+                    'form' => $form->createView(),
         ]);
     }
 
